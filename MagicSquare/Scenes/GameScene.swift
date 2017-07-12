5e833231830237d8195affe2084add1c72c3a578
athosLagemann <athoslagemann@gmail.com>
2017-07-12 13:50:24 -0300

    Temporary fix

--- MagicSquare/Scenes/GameScene.swift
+++ MagicSquare/Scenes/GameScene.swift
@@ -12,22 +12,21 @@ import GameplayKit
 class GameScene: SKScene {
 // MARK: - Properties
 	
+//	private var gameWorld : World!
 	private var currentLevel : Level!
 	
 // MARK: - Methods
 	
 	override func didMove(to view: SKView) {
 		
-		currentLevel = Level()
-		currentLevel.rows = 4
-		currentLevel.columns = 4
+		currentLevel = World.loadLevel(numberOfLevel: 1)
+
+//		let cellHorizontalSpacing = ((self.scene?.size.width)! / CGFloat(currentLevel.rows))
+//		let cellVerticalSpacing = ((self.scene?.size.height)!*0.7 / CGFloat(currentLevel.columns))
+//		print("cHS: \(cellHorizontalSpacing), cVS: \(cellVerticalSpacing)")
+//		print("iphone7: \(self.scene?.size.width ?? 0) x \(self.scene?.size.height ?? 0)")
 		
-		let cellHorizontalSpacing = ((self.scene?.size.width)! / CGFloat(currentLevel.rows))
-		let cellVerticalSpacing = ((self.scene?.size.height)!*0.7 / CGFloat(currentLevel.columns))
-		print("cHS: \(cellHorizontalSpacing), cVS: \(cellVerticalSpacing)")
-		print("iphone7: \(self.scene?.size.width ?? 0) x \(self.scene?.size.height ?? 0)")
-		
-		placeBoardCells(cellHorizontalSpacing, cellVerticalSpacing)
+//		placeBoardCells(cellHorizontalSpacing, cellVerticalSpacing)
 	}
 	
 	override func update(_ currentTime: TimeInterval) {
@@ -40,14 +39,14 @@ class GameScene: SKScene {
 		var currentX = Xspacing
 		var currentY = Yspacing
 		
-		let cell: BoardCell!
-		cell = BoardCell(color: UIColor.gray)
+//		let cell: BoardCell!
+//		cell = BoardCell(color: UIColor.gray)
 //		for cell in currentLevel.playerBoard.cellsMatrix {
 		
-		let square = cell.getSpriteNode()
-		square.position = CGPoint(x: currentX, y: currentY)
-		print("pos: \(square.position)")
-		self.addChild(square)
+//		let square = cell.getSpriteNode()
+//		square.position = CGPoint(x: currentX, y: currentY)
+//		print("pos: \(square.position)")
+//		self.addChild(square)
 		
 		currentY += Yspacing
 		currentX += Xspacing
