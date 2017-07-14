4c8a2284d1844e8158fd1e126516f74dead0a141
Unknown <marcelo.andrighetto@hotmail.com>
2017-07-14 16:51:28 -0300

    Levels scene with main menu scene in background

--- MagicSquare/Scenes/LevelsScene.swift
+++ MagicSquare/Scenes/LevelsScene.swift
@@ -10,5 +10,27 @@ import SpriteKit
 import GameplayKit
 
 class LevelsScene: SKScene {
-
+    var previousSceneChildren: SKSpriteNode!
+    var backgroundScreen: SKSpriteNode!
+    
+    func prepareScene(from previousScene: SKScene) {
+        previousSceneChildren = SKSpriteNode(color: UIColor.white, size: (previousScene.view?.bounds.size)!)
+        previousScene.zPosition = 0
+        
+        for child in previousScene.children {
+            child.removeFromParent()
+            previousSceneChildren.addChild(child)
+        }
+        
+        addChild(previousSceneChildren)
+    }
+    
+    override func didMove(to view: SKView) {
+        let backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0.2)
+        backgroundScreen = SKSpriteNode(color: backgroundColor, size: view.bounds.size)
+        backgroundScreen.zPosition = 1
+        addChild(backgroundScreen)
+        
+        
+    }
 }
