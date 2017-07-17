65ce7d7d58b0645f9c376db2e448c968a4c6768e
Unknown <marcelo.andrighetto@hotmail.com>
2017-07-17 18:09:04 -0300

    Main screen test description added

--- MagicSquare/Scenes/MainMenuScene.swift
+++ MagicSquare/Scenes/MainMenuScene.swift
@@ -11,11 +11,23 @@ import GameplayKit
 
 class MainMenuScene: SKScene {
     var backgroundColorNode: SKSpriteNode!
+    var titleLabelNode: SKLabelNode!
     
     override func didMove(to view: SKView) {
         backgroundColorNode = SKSpriteNode(color: UIColor.cyan, size: view.bounds.size)
         backgroundColorNode.name = "Background Color"
+        backgroundColorNode.zPosition = 0
         addChild(backgroundColorNode)
+        
+        titleLabelNode = SKLabelNode(text: "TAP TO BEGIN!")
+        titleLabelNode.fontColor = UIColor.black
+        titleLabelNode.fontName = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize).fontName
+        titleLabelNode.zPosition = 0.5
+        let increaseScale = SKAction.scale(by: 1.1, duration: 0.5)
+        let decreaseScale = SKAction.scale(to: 1, duration: 0.5)
+        let animation = SKAction.repeatForever(SKAction.sequence([increaseScale, decreaseScale]))
+        titleLabelNode.run(animation)
+        addChild(titleLabelNode)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
