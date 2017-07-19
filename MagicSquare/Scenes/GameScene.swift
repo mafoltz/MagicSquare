dc082c784facb33f7b383387af2451165a8e3576
arthurgiachini97 <arthur.giachini@acad.pucrs.br>
2017-07-19 18:29:02 -0300

    Moves != 0 verification

--- MagicSquare/Scenes/GameScene.swift
+++ MagicSquare/Scenes/GameScene.swift
@@ -558,19 +558,19 @@ class GameScene: SKScene {
             }
             
             
-            if direction == .vertical && firstTouch.y < lastTouch.y && column >= 0 {
+            if direction == .vertical && firstTouch.y < lastTouch.y && column >= 0 && moves != 0 {
                 print(column)
                 currentLevel.moveUpPlayerBoard(column: column - 1, moves: abs(moves))
                 //print(moves)
-            } else if direction == .vertical && firstTouch.y > lastTouch.y && column >= 0{
+            } else if direction == .vertical && firstTouch.y > lastTouch.y && column >= 0 && moves != 0{
                 print(column)
                 currentLevel.moveDownPlayerBoard(column: column - 1, moves:abs(moves))
                 //print(moves)
-            } else if direction == .horizontal && firstTouch.x < lastTouch.x && row >= 0 {
+            } else if direction == .horizontal && firstTouch.x < lastTouch.x && row >= 0 && moves != 0 {
                 print(row)
                 //print(moves)
                 currentLevel.moveRightPlayerBoard(row: row - 1, moves: abs(moves))
-            } else if direction == .horizontal && firstTouch.x > lastTouch.x && row >= 0 {
+            } else if direction == .horizontal && firstTouch.x > lastTouch.x && row >= 0 && moves != 0 {
                 print(row)
                 currentLevel.moveLeftPlayerBoard(row: row - 1, moves: abs(moves))
                 //print(moves)
