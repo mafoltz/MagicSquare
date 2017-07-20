37a93575606cc4bf640593d255db2f22b7fb4f2f
Unknown <marcelo.andrighetto@hotmail.com>
2017-07-20 18:50:30 -0300

    Scenes navigation after wins

--- MagicSquare/Scenes/ResultsScene.swift
+++ MagicSquare/Scenes/ResultsScene.swift
@@ -10,5 +10,26 @@ import SpriteKit
 import GameplayKit
 
 class ResultsScene: SKScene {
-
+    
+    public var currentLevel : Level?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let json: [[String: Any]] = JsonReader.openJson(named: "World")!
+        if (currentLevel?.number)! < json.count - 1 {
+            let scene: GameScene = GameScene()
+            scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            scene.size = (super.view?.bounds.size)!
+            scene.scaleMode = .aspectFill
+            scene.currentLevel = JsonReader.loadLevel(from: json, numberOfLevel: (currentLevel?.number)! + 1)
+            super.view?.presentScene(scene)
+        }
+        else {
+            let scene: LevelsScene = LevelsScene()
+            scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            scene.size = (super.view?.bounds.size)!
+            scene.scaleMode = .aspectFill
+            scene.prepareScene(from: self.scene!)
+            super.view?.presentScene(scene)
+        }
+    }
 }
