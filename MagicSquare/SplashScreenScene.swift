e34cee678104a7d06f9dda7d5f2bcedf54492050
Unknown <marcelo.andrighetto@hotmail.com>
2017-07-26 19:26:48 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ MagicSquare/SplashScreenScene.swift
@@ -0,0 +1,15 @@
+//
+//  SplashScreenScene.swift
+//  MagicSquare
+//
+//  Created by Marcelo Andrighetto Foltz on 26/07/17.
+//  Copyright © 2017 Marcelo Andrighetto Foltz. All rights reserved.
+//
+
+import SpriteKit
+
+class SplashScreenScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
